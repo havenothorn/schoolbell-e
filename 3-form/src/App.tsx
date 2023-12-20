dce7f97b668645dccc04f5c1745ae916863d984f
Nami <havenothorn@gmail.com>
2023-12-20 22:20:39 +0900

    add: third task setting

---
+++ 3-form/src/App.tsx
@@ -0,0 +1,28 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./App.css";
+
+enum GenderEnum {
+  female = "female",
+  male = "male",
+  other = "other",
+}
+
+interface IFormInput {
+  firstName: string;
+  gender: GenderEnum;
+}
+
+function App() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First Name</label>
+      <input {...register("firstName")} />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default App;
